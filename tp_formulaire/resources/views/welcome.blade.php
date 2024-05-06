65a58ae01af845d043a404a5a18ff5705bf7a3af
hezou <hezoubeke18@gmail.com>
2024-05-06 06:58:44 +0000

    project init

---
+++ tp_formulaire/resources/views/welcome.blade.php
@@ -0,0 +1,36 @@
+
+<head>
+  <script src="https://cdn.tailwindcss.com"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
+</head>
+  <section class="bg-gray-50 dark:bg-gray-900">
+  <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+      <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+          Formulaire d'enregistrement   
+      </a>
+      <div class="w-full p-6 bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md dark:bg-gray-800 dark:border-gray-700 sm:p-8">
+          
+          <form   method="POST" class="mt-4 space-y-4 lg:mt-5 md:space-y-5" action="" >
+          <div>
+                  <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"> Nom </label>
+                  <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder=" saisir votre nom " required="">
+              </div>  <div>
+                  <label for="first_namr" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"> prénom</label>
+                  <input type="text" name="first_name" id="first_namr" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="saisir votre prénom" required="">
+              </div>
+              <div>
+                  <label for="age" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Age</label>
+                  <input type="number" name="item-weight" id="item-weight" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="0" required="">
+              </div> 
+              <div>
+                  <label for="sexe " class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Sexe </label>
+                  <select id="sexe " class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                      <option selected="">M</option>
+                      <option value="F">F</option>
+                  </select>
+              </div>
+              <button type="submit" class="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800  ">valider </button>
+          </form>
+      </div>
+  </div>
+</section>
